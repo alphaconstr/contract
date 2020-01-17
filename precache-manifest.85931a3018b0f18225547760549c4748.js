a79cd1ae0e847a984945a05d978864e429e401a4
alphaconstr <59947746+alphaconstr@users.noreply.github.com>
2020-01-17 09:26:31 +0200

    zuzu

---
+++ precache-manifest.85931a3018b0f18225547760549c4748.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09437f9898ddb71d90a17a3001ac103f",
+    "url": "/newapp/index.html"
+  },
+  {
+    "revision": "86ee61c530bd89326e76",
+    "url": "/newapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6b9b7ced6fbe7595e253",
+    "url": "/newapp/static/js/2.98d3231e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newapp/static/js/2.98d3231e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86ee61c530bd89326e76",
+    "url": "/newapp/static/js/main.ed76a413.chunk.js"
+  },
+  {
+    "revision": "11d5570fc37a86eb0151",
+    "url": "/newapp/static/js/runtime-main.5ab712ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
